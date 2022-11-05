bb0e208f9e985af17721b92f4ff3d7692e57457f
lance <q161162@163.com>
2022-11-05 09:51:13 +0800

    修改dockerfile，自动安装requirement.txt中依赖;修改docker-compose.yml,增加env

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from typing import Union
 import uvicorn
 from fastapi import FastAPI
+from loguru import logger as log
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    log.info("你好")
     return {"Hello": "World2"}
+
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    log.info(f"items:{item_id}")
     return {"item_id": item_id, "q": q}
 
 
 if __name__=="__main__":
+    log.info("server start")
     uvicorn.run("main:app", host="0.0.0.0", port=8004, reload=True)
