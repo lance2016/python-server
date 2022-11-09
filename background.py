08fda97cc142cae25d8a4fc08287069c75d33a38
feilong Chen <chenfeilong@cloudwalk.com>
2022-11-09 10:19:23 +0800

    修改后台启动文件

--- background.py
+++ background.py
@@ -15,13 +15,12 @@ def read_root():
     return {"Hello": "World2"}
 
 
+@app.get("/ping")
+def read_item():
+    log.info("connected")
+    return "connected successful"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    log.info(f"items:{item_id}")
-    return {"item_id": item_id, "q": q}
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     log.info("server start")
-    uvicorn.run("main:app", host="0.0.0.0", port=18000, reload=True)
+    uvicorn.run("background:app", host="0.0.0.0", port=18000, reload=True)
