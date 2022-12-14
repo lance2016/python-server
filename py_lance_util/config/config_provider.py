85c487caf56d871174415b9d88a62806f819caba
feilong Chen <chenfeilong@cloudwalk.com>
2022-12-14 22:47:09 +0800

    部分适配nacos, rabbitmq通过nacos读取配置

---
+++ py_lance_util/config/config_provider.py
@@ -0,0 +1,45 @@
+import configparser
+from loguru import logger
+import os
+from functools import lru_cache
+import nacos
+
+ENV = os.getenv("env", "dev")
+NACOS_ADDRESS = "49.234.42.199:8848"
+NAMESPACE_ID = "eef4e483-a2b9-455a-b367-8bdf8f451aed"
+GROUP = "DEV"
+# !!!!此处namesace为Namespace ID
+client = nacos.NacosClient(NACOS_ADDRESS, namespace=NAMESPACE_ID)
+
+
+class ConfigProvider:
+
+    def __init__(self, appname: str):
+        self.config_data_id = f'{appname}-{ENV}'
+        logger.info(f'config_data_id: {self.config_data_id}')
+        try:
+            config_raw = client.get_config(self.config_data_id, group=GROUP)
+            self.config = configparser.ConfigParser()
+            self.config.read_string(config_raw)
+            logger.info(f'config: {self.config}')
+        except Exception as e:
+            logger.error(f'get config failed: {e}')
+
+    def get_config(self, section: str, key: str):
+        return self.config[section][key]
+
+    def get_section(self, section: str):
+        return self.config[section]
+
+
+@lru_cache()
+def get_config(appname) -> ConfigProvider:
+    return ConfigProvider(appname)
+
+
+if __name__ == "__main__":
+    config = ConfigProvider("dp-python-server")
+    rabbit_mq = config.get_section("rabbit_mq")
+    print(rabbit_mq["user"])
+    print(rabbit_mq["password"])
+    print(rabbit_mq["host"])
--- py_lance_util/py_practice/service/mq_service.py
+++ py_lance_util/py_practice/service/mq_service.py
@@ -2,19 +2,22 @@
 
 from loguru import logger as log
 from config.config import get_settings
+from py_lance_util.config.config_provider import get_config
 from utils.rabbitmq_util import RabbitMQ
 
-setting = get_settings()
+config = get_config("dp-python-server")
+rabbitmq_config = config.get_section("rabbit_mq")
+
 
 def rabbitmq_produce(params: dict):
     try:
         # Create the RabbitMQ object
         rmq = RabbitMQ(
-            host=setting.rabbit_mq_host,
-            port=setting.rabbit_mq_port,
-            vhost=setting.rabbit_mq_vhost,
-            username=setting.rabbit_mq_user,
-            password=setting.rabbit_mq_psw
+            host=rabbitmq_config["host"],
+            port=rabbitmq_config["port"],
+            vhost=rabbitmq_config["vhost"],
+            username=rabbitmq_config["user"],
+            password=rabbitmq_config["password"]
         )
         exchange = 'exchange_hello'
         queue = 'queue_hello'
